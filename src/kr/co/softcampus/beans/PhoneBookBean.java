deb28f7afe02fb919ebb421e2460fd2ce19aa8b3
boyouth <qhdudqkra@gmail.com>
2021-05-19 22:02:29 +0900

    전화번호부 DTO

---
+++ src/kr/co/softcampus/beans/PhoneBookBean.java
@@ -0,0 +1,54 @@
+package kr.co.softcampus.beans;
+
+
+public class PhoneBookBean {
+	
+	private int list_idx;
+	private String list_depart;
+	private String list_writer_id;
+	private String list_content;
+	private String list_writer_name;	
+	private String list_date;
+	
+	public int getList_idx() {
+		return list_idx;
+	}
+	public void setList_idx(int list_idx) {
+		this.list_idx = list_idx;
+	}
+	public String getList_depart() {
+		return list_depart;
+	}
+	public void setList_depart(String list_depart) {
+		this.list_depart = list_depart;
+	}
+	public String getList_writer_id() {
+		return list_writer_id;
+	}
+	public void setList_writer_id(String list_writer_id) {
+		this.list_writer_id = list_writer_id;
+	}
+	public String getList_writer_name() {
+		return list_writer_name;
+	}
+	public void setList_writer_name(String list_writer_name) {
+		this.list_writer_name = list_writer_name;
+	}
+	public String getList_date() {
+		return list_date;
+	}
+	public void setList_date(String list_date) {
+		this.list_date = list_date;
+	}
+	public String getList_content() {
+		return list_content;
+	}
+	public void setList_content(String list_content) {
+		this.list_content = list_content;
+	}
+	
+	
+	
+
+	
+}
