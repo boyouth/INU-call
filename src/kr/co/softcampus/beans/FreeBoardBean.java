9e64ff8dae72e8a61fd3c49ada0bed314f678354
boyouth <qhdudqkra@gmail.com>
2021-05-20 22:06:47 +0900

    자유게시판 빈 수정

---
+++ src/kr/co/softcampus/beans/FreeBoardBean.java
@@ -0,0 +1,52 @@
+package kr.co.softcampus.beans;
+
+public class FreeBoardBean {
+	
+	private int free_idx;
+	private String free_title;
+	private String free_content;
+	private String free_writer_id;
+	private String free_writer_name;
+	private String free_date;
+	
+	
+	public int getFree_idx() {
+		return free_idx;
+	}
+	public void setFree_idx(int free_idx) {
+		this.free_idx = free_idx;
+	}
+	public String getFree_title() {
+		return free_title;
+	}
+	public void setFree_title(String free_title) {
+		this.free_title = free_title;
+	}
+	public String getFree_content() {
+		return free_content;
+	}
+	public void setFree_content(String free_content) {
+		this.free_content = free_content;
+	}
+	public String getFree_writer_id() {
+		return free_writer_id;
+	}
+	public void setFree_writer_id(String free_writer_id) {
+		this.free_writer_id = free_writer_id;
+	}
+	public String getFree_writer_name() {
+		return free_writer_name;
+	}
+	public void setFree_writer_name(String free_writer_name) {
+		this.free_writer_name = free_writer_name;
+	}
+	public String getFree_date() {
+		return free_date;
+	}
+	public void setFree_date(String free_date) {
+		this.free_date = free_date;
+	}
+	
+	
+
+}
