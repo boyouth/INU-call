c5ba278b8e77e69c48f467bb50f9b258da80c642
boyouth <qhdudqkra@gmail.com>
2021-05-20 22:06:58 +0900

    문의게시판 빈 추가

---
+++ src/kr/co/softcampus/beans/InqBoardBean.java
@@ -0,0 +1,57 @@
+package kr.co.softcampus.beans;
+
+public class InqBoardBean {
+	
+	private int inquire_idx;
+	private String inquire_type;
+	private String inquire_title;
+	private String inquire_content;
+	private String inquire_writer_id;
+	private String inquire_writer_name;
+	private String inquire_date;
+	
+	public int getInquire_idx() {
+		return inquire_idx;
+	}
+	public void setInquire_idx(int inquire_idx) {
+		this.inquire_idx = inquire_idx;
+	}
+	public String getInquire_type() {
+		return inquire_type;
+	}
+	public void setInquire_type(String inquire_type) {
+		this.inquire_type = inquire_type;
+	}
+	public String getInquire_title() {
+		return inquire_title;
+	}
+	public void setInquire_title(String inquire_title) {
+		this.inquire_title = inquire_title;
+	}
+	public String getInquire_content() {
+		return inquire_content;
+	}
+	public void setInquire_content(String inquire_content) {
+		this.inquire_content = inquire_content;
+	}
+	public String getInquire_writer_id() {
+		return inquire_writer_id;
+	}
+	public void setInquire_writer_id(String inquire_writer_id) {
+		this.inquire_writer_id = inquire_writer_id;
+	}
+	public String getInquire_writer_name() {
+		return inquire_writer_name;
+	}
+	public void setInquire_writer_name(String inquire_writer_name) {
+		this.inquire_writer_name = inquire_writer_name;
+	}
+	public String getInquire_date() {
+		return inquire_date;
+	}
+	public void setInquire_date(String inquire_date) {
+		this.inquire_date = inquire_date;
+	}
+	
+	
+}
