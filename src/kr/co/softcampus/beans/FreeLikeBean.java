3bfe88e3d877be524bcc24c841545e1400985170
boyouth <qhdudqkra@gmail.com>
2021-06-03 19:51:18 +0900

    좋아요 구현을 위한 빈 생성

---
+++ src/kr/co/softcampus/beans/FreeLikeBean.java
@@ -0,0 +1,29 @@
+package kr.co.softcampus.beans;
+
+public class FreeLikeBean {
+	private int free_idx;
+	private String user_id;
+	private int likey;
+	
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getLikey() {
+		return likey;
+	}
+	public void setLikey(int likey) {
+		this.likey = likey;
+	}
+	public int getFree_idx() {
+		return free_idx;
+	}
+	public void setFree_idx(int free_idx) {
+		this.free_idx = free_idx;
+	}
+	
+
+}
